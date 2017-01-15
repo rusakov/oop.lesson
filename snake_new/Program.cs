a4725c476ebfc69ec9d83de79b68703c50a63a5b
Konstantin Rusakov <rusakov.k@gmail.com>
2017-01-15 21:26:15 +0300

    lesson 06

--- snake_new/Program.cs
+++ snake_new/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake_new
 {
@@ -6,12 +7,24 @@ namespace snake_new
 	{
 		public static void Main(string[] args)
 		{
-
+				
 			Point p1 = new Point(1, 3, '#');
-			p1.Draw();
+			Point p2 = new Point(4, 5, '*');
+			Point p3 = new Point(12, 10, '⁄');
+			Point p4 = new Point(p3.x * 3, p3.y * 2, '€');
 
-			Point p2 = new Point(4, 5, '#');
-			p2.Draw();
+			List<Point> pList = new List<Point>();
+
+			pList.Add(p1);
+			pList.Add(p2);
+			pList.Add(p3);
+			pList.Add(p4);
+
+
+			foreach (var i in pList) {
+				i.Draw();
+			}
+
 		}
 	}
 }
