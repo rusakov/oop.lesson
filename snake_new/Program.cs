d5f00a66e26348b0ad740575ed772557d6075e08
Konstantin Rusakov <rusakov.k@gmail.com>
2017-01-15 22:00:17 +0300

    lesson 07

--- snake_new/Program.cs
+++ snake_new/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 
 namespace snake_new
@@ -6,25 +7,18 @@ namespace snake_new
 	class MainClass
 	{
 		public static void Main(string[] args)
-		{
-				
-			Point p1 = new Point(1, 3, '#');
-			Point p2 = new Point(4, 5, '*');
-			Point p3 = new Point(12, 10, '⁄');
-			Point p4 = new Point(p3.x * 3, p3.y * 2, '€');
+		{				
+			Point p1 = new Point(1, 3, '*');
+			p1.Draw();
 
-			List<Point> pList = new List<Point>();
+			Point p2 = new Point(4, 5, '#');
+			p2.Draw();
 
-			pList.Add(p1);
-			pList.Add(p2);
-			pList.Add(p3);
-			pList.Add(p4);
+			HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+			line.Drow();
 
-
-			foreach (var i in pList) {
-				i.Draw();
-			}
-
+			VerticalLine line1 = new VerticalLine(2, 10, 4, '&');
+			line1.Drow();
 		}
 	}
 }
---
+++ snake_new/VerticalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace snake_new
+{
+	public class VerticalLine
+	{
+		List<Point> pList;
+
+		public VerticalLine(int yTop, int yBottom, int x, char sym)
+		{
+			pList = new List<Point>();
+
+			for (int y = yTop; y <= yBottom; y++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+		}
+
+		public void Drow()
+		{
+
+			foreach (Point p in pList)
+			{
+				p.Draw();
+			}
+
+		}
+	}
+}
