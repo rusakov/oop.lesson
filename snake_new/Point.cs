5f4aeed78353e4f713283d091015f2aa43d9eed1
Konstantin Rusakov <rusakov.k@gmail.com>
2017-01-15 20:47:52 +0300

    lesson 03

--- snake_new/Point.cs
+++ snake_new/Point.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+
 namespace snake_new
 {
 	public class Point
 	{
-		public Point()
-		{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw() { 		
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
+			Console.ReadLine();			
 		}
 	}
 }
